38fdd989f159070bfff08e0c20c4bb4254a054b4
Billy-Crawford <oumaroubilly6@gmail.com>
2026-05-06 07:57:40 +0000

    api pret pour vercel

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "https://mbh-chaincacao-back.onrender.com",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://mbh-chaincacao-back.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
